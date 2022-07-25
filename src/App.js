04902512e0b12369a4d68de70fa0ee76dfd15f09
Katelyn Chambers <kchambersofficial@yahoo.com>
2022-07-24 21:35:59 -0700

    pokemonapi_assign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState ([]);
+
+  const onClickHandler = () =>{
+    console.log("calling API...");
+
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+      .then(response => {
+        let results = response.data.results.map( p => p.name)
+        setPokemon(results)
+      })
+      .catch(err => console.log(err))
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <h1>Pokemon: Catch Them All!</h1> */}
+      <div className="button-row">
+        <button onClick={()=>onClickHandler()} >Fetch Pokemon!</button>
+      </div>
+      <ul>
+        {
+          pokemon.map((item, i) => {
+            return <li key={i}> {item } </li>
+          })   
+        }
+      </ul>
+
+
     </div>
   );
 }
+
 
 export default App;
